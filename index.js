95611ac149afc08e3b1db35e03a2617765dec9b2
Rodrigo Suman <rodrigosuman@pop-os.localdomain>
2021-12-13 16:23:37 -0300

    commit inicial: setup com react-native init e typescript

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import ProductList from './src/screens/ProductList/ProductList';
+
+AppRegistry.registerComponent(appName, () => ProductList);
